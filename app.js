731ca6410b0c6795758724a1a254ca16dc8b630b
cody <codyjeromegreen@gmail.com>
2017-10-12 10:40:14 -0700

    third store done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 //create an array of open store hours
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 //First and Pike Location
 var firstAndPike = {
@@ -98,3 +98,48 @@ var seatacairport = {
 };
 
 seatacairport.render();
+
+var seattlecenter = {
+  name: 'Seattle Center',
+  //creating my key value pairs inside of my object they are called properties
+  minCustPerHour: 11,
+  maxCustPerHour: 38,
+  avgCookieSoldPerHour: 3.7,
+  randCustByHour: [],
+  cookiesSoldByHour: [],
+  totalCookies: 0,
+  //method for random customers by hour
+  calcRandCustByHour: function() {
+    for(var o = 0; o < hours.length; o++) {
+      this.randCustByHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
+      console.log(this.randCustByHour[o]);
+
+    }
+  },
+  //method for cookies sold by hours
+  calcCookiesSoldByHour: function() {
+    for(var p = 0; p < hours.length; p++) {
+      this.cookiesSoldByHour.push(Math.round(this.avgCookieSoldPerHour * this.randCustByHour[p]));
+      console.log(this.cookiesSoldByHour[p]);
+    }
+  },
+  render: function() {
+    var centerli = document.getElementById('centerli');
+    var seaCenter = document.getElementById('seaCenter');
+    this.calcRandCustByHour();
+    this.calcCookiesSoldByHour();
+
+    var h3El = document.createElement('h3');
+
+    h3El.textContent = this.name;
+    seaCenter.appendChild(h3El);
+    for(var q = 0; q < hours.length; q++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[q] + ': ' + this.cookiesSoldByHour[q] + ' cookies';
+      console.log(liEl);
+      centerli.appendChild(liEl);
+    }
+  }
+};
+
+seattlecenter.render();
